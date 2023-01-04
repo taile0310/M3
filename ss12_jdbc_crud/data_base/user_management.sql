a7ec2dea60b758a05b7f5718d77c07c08635365a
lehuutai0310 <lehuutai0310@gmail.com>
2023-01-04 21:31:33 +0700

    data_base

--- ss12_jdbc_crud/data_base/user_management.sql
+++ ss12_jdbc_crud/data_base/user_management.sql
@@ -1,2 +1,17 @@
 create database user_management;
-use data_base;
+use user_management;
+
+create table users (
+id int(3) not null auto_increment primary key,
+user_name varchar(120) not null,
+user_email varchar(120) not null,
+country varchar(120)
+);
+
+insert into users (user_name, user_email, country) values
+('Minh','minh@gmail.com','VietNam'),
+('Kante','kante@gmail.com','Canada'),
+('Shinzao','shinzao@gmail.com','China'),
+('Tomy','tomy@gmail.com','England');
+
+select * from users;
